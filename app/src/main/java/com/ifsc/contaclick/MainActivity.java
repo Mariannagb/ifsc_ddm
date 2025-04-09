94b198fb6843cd390ce2c34401395eb36a7c4c4c
mariannagb <marianna.mgb@gmail.com>
2025-04-09 13:00:01 -0300

    Adiciona projeto

---
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ifsc.contaclick;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity
+implements View.OnClickListener {
+    TextView textView;
+    Button button;
+    int contador = 0;2
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.clickCountText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador++;
+                textView.setText(Integer.toString(contador));
+            }
+        });
+    }
+
+    public void onClick(View view){
+
+    }
+}
