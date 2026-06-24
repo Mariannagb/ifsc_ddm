4f66e6b63c0212bc3f2d1abd1683eb187c760754
Mariannagb <marianna.mgb@gmail.com>
2026-06-24 01:58:54 -0300

    Corrige bug do fechamento do app e ajusta lógica do imc

--- app/src/main/java/com/example/ifsc_ddm/MainActivity.java
+++ app/src/main/java/com/example/ifsc_ddm/MainActivity.java
@@ -1,23 +1,43 @@
 package com.example.ifsc_ddm;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends Activity {
+    Button button;
+    EditText altura;
+    EditText peso;
 
-import java.util.Random;
-
-public class MainActivity extends AppCompatActivity {
-    EditText edmin, edmax;
-    TextView tvResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        altura = findViewById(R.id.altura);
+        peso = findViewById(R.id.peso);
+
+        button.setOnClickListener(v -> {
+            try {
+                String stringPeso = peso.getText().toString().replace(",", ".");
+                String stringAltura = altura.getText().toString().replace(",", ".");
+
+                float valorPeso = Float.parseFloat(stringPeso);
+                float valorAltura = Float.parseFloat(stringAltura);
+
+                float imc = valorPeso / (valorAltura * valorAltura);
+
+                Intent i = new Intent(getApplicationContext(), IMC.class);
+                i.putExtra("msg", imc);
+                startActivity(i);
+
+            } catch (NumberFormatException e) {
+                Toast.makeText(this, "Preencha o peso e a altura corretamente", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
-
 }
