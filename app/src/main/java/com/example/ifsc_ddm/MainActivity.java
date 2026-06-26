3cd6322dcf9810090909dd63a08091ff30de1810
Mariannagb <marianna.mgb@gmail.com>
2026-06-25 22:57:35 -0300

    Codigo revisado

--- app/src/main/java/com/example/ifsc_ddm/MainActivity.java
+++ app/src/main/java/com/example/ifsc_ddm/MainActivity.java
@@ -1,23 +1,60 @@
 package com.example.ifsc_ddm;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
-    EditText edmin, edmax;
-    TextView tvResultado;
+
+    Button buttonAvancar, buttonVoltar, buttonMenu;
+    ImageView imageView;
+    int imagens[] = new int[]{R.drawable.cachorro,
+            R.drawable.gardem,
+            R.drawable.happy,
+            R.drawable.patinho,
+            R.drawable.porquinho};
+
+    int posicao = 0;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAvancar = findViewById(R.id.btnAvancar);
+        buttonVoltar = findViewById(R.id.btnVoltar);
+        buttonMenu = findViewById(R.id.btnMenu);
+        imageView = findViewById(R.id.imageView);
+
+        buttonAvancar.setOnClickListener(v -> {
+            posicao++;
+            if (posicao >= imagens.length) {
+                posicao = 0;
+            }
+            imageView.setImageResource(imagens[posicao]);
+        });
+
+        buttonVoltar.setOnClickListener(v -> {
+            posicao--;
+            if (posicao < 0) {
+                posicao = imagens.length - 1;
+            }
+            imageView.setImageResource(imagens[posicao]);
+        });
+
+        buttonMenu.setOnClickListener(v -> {
+            finish();
+        });
     }
 
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        posicao = 0;
+        imageView.setImageResource(imagens[0]);
+    }
 }
